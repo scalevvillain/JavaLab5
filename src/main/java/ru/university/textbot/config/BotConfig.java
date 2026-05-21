5e93d934ff3aa1906ecae8cb38a7e664fb9ffd9d
Andrey Nikitchenko <shihki@inbox.ru>
2026-05-21 22:12:53 +0300

    Update BotConfig.java

--- src/main/java/ru/university/textbot/config/BotConfig.java
+++ src/main/java/ru/university/textbot/config/BotConfig.java
@@ -2,7 +2,7 @@ package ru.university.textbot.config;
 
 public class BotConfig {
 
-    public static final String BOT_USERNAME = "YourBotName";
+    public static final String BOT_USERNAME = "LabJava5";
 
     // Токен, полученный от @BotFather
     public static final String BOT_TOKEN = "Вставить_Токен_Сюда";
